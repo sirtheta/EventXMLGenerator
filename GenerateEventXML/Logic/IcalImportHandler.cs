1e918057429d5facc752f7d931aa4a51d74b35b8
sirtheta <sirtheta@gmail.com>
2026-04-03 09:57:20 +0200

    add replacement for keyword GD

--- GenerateEventXML/Logic/IcalImportHandler.cs
+++ GenerateEventXML/Logic/IcalImportHandler.cs
@@ -3,43 +3,38 @@ using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace GenerateEventXML.Logic
 {
-  internal static class IcalImportHandler
-  {
-    private const string _importTimeFormat = "dd-MM-yyyy HH:mm";
-    /// <summary>
-    /// Importfunction for the ics file, will import all entrys starting with the given 
-    /// keyword in the event description from the given start date on
-    /// </summary>
-    /// <param name="icalText"></param>
-    /// <param name="startTime"></param>
-    /// <returns></returns>
-    internal static IEnumerable<Event> ImportFile(string icalText, DateTime startTime, string keyword)
+    internal static partial class IcalImportHandler 
     {
-      List<Event> events = new();
+        private const string _importTimeFormat = "dd-MM-yyyy HH:mm";
 
-      var calendar = Ical.Net.Calendar.Load(icalText);
+        [GeneratedRegex(@"\bGD\b")]
+        private static partial Regex GdRegex();
 
-      foreach (var calendarEvent in calendar.Events)
-      {
-        if (calendarEvent.DtStart.Value < startTime)
-          continue;
-
-        if (calendarEvent.Description?.StartsWith(keyword) == true)
+        internal static IEnumerable<Event> ImportFile(string icalText, DateTime startTime, string keyword)
         {
-          var mappedEvent = new Event
-          {
-            EventTitle = calendarEvent.Summary,
-            DateTimeStart = calendarEvent.DtStart.Value.ToString(_importTimeFormat),
-            DateTimeEnd = calendarEvent.DtEnd.Value.ToString(_importTimeFormat),
-          };
-          events.Add(mappedEvent);
-        }
-      }
+            List<Event> events = [];
+            var calendar = Ical.Net.Calendar.Load(icalText);
+            foreach (var calendarEvent in calendar!.Events)
+            {
+                if (calendarEvent.DtStart!.Value < startTime)
+                    continue;
+                if (calendarEvent.Description?.StartsWith(keyword) == true)
+                {
+                    var mappedEvent = new Event
+                    {
+                        EventTitle = GdRegex().Replace(calendarEvent.Summary ?? "", "Gottesdienst"),
+                        DateTimeStart = calendarEvent.DtStart.Value.ToString(_importTimeFormat),
+                        DateTimeEnd = calendarEvent.DtEnd!.Value.ToString(_importTimeFormat),
+                    };
 
-      return events.OrderBy(e => DateTime.ParseExact(e.DateTimeStart, _importTimeFormat, CultureInfo.InvariantCulture));
+                    events.Add(mappedEvent);
+                }
+            }
+            return events.OrderBy(e => DateTime.ParseExact(e.DateTimeStart, _importTimeFormat, CultureInfo.InvariantCulture));
+        }
     }
-  }
 }
